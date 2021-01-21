def8b53dc959a2c81b0abb1ffdf7b7a66dbb2484
Anders Pitman <tapitman11@gmail.com>
2021-01-21 15:55:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+export const backendMap = {
+  "https://mosaic.chpc.utah.edu":         "mosaic.chpc.utah.edu/gru/api/v1",
+  "https://mosaic-staging.chpc.utah.edu": "mosaic-staging.chpc.utah.edu/gru/api/v1",
+  "https://cddrc.utah.edu":                "mosaic.chpc.utah.edu/gru/api/v1",
+  "https://mosaic-dev.genetics.utah.edu": "mosaic.chpc.utah.edu",
+  "http://mosaic-dev.genetics.utah.edu":  "mosaic.chpc.utah.edu",
+};
