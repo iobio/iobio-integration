6504dc098a5089f86d78bfc46f4ac365dd45dc36
Anders Pitman <tapitman11@gmail.com>
2021-01-22 15:30:11 -0700

    Switch to class-based approach
    Each data source can now have multiple backend options. This is
    nice for things like testing against a dev or staging backend.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-export const backendMap = {
-  "https://mosaic.chpc.utah.edu":         "mosaic.chpc.utah.edu/gru/api/v1",
-  "https://mosaic-staging.chpc.utah.edu": "mosaic-staging.chpc.utah.edu/gru/api/v1",
-  "https://cddrc.utah.edu":                "mosaic.chpc.utah.edu/gru/api/v1",
-  "https://mosaic-dev.genetics.utah.edu": "mosaic.chpc.utah.edu",
-  "http://mosaic-dev.genetics.utah.edu":  "mosaic.chpc.utah.edu",
+const backendMap = {
+  "https://mosaic.chpc.utah.edu": [
+    "mosaic.chpc.utah.edu/gru/api/v1",
+    "mosaic.chpc.utah.edu/gru-dev",
+  ],
+  "https://cddrc.utah.edu": [
+    "mosaic.chpc.utah.edu/gru/api/v1",
+    "mosaic.chpc.utah.edu/gru-dev",
+  ],
+  "https://mosaic-staging.chpc.utah.edu": [
+    "mosaic-staging.chpc.utah.edu/gru/api/v1",
+  ],
 };
+
+export function createBackendManager(dataSourceUrl) {
+  if (!backendMap[dataSourceUrl]) {
+    return new BackendManager(['backend.iobio.io']);
+  }
+
+  return new BackendManager(backendMap[dataSourceUrl]);
+}
+
+class BackendManager {
+  constructor(backends) {
+    this.backends = backends;
+  }
+
+  defaultBackend() {
+    return this.backends[0];
+  }
+
+  getBackend(backend) {
+    if (this.backends.includes(backend)) {
+      return backend;
+    }
+    else {
+      return this.defaultBackend();
+    }
+  }
+}
